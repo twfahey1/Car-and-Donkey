ea0e5302cfdbecae19b23db81d299f8ed6b35d62
Tyler Fahey <twfahey@me.com>
2015-12-05 10:45:01 -0500

    First commit working

---
+++ Car and Donkey/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Car_and_Donkey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.resultsList = new System.Windows.Forms.ListBox();
+            this.activityLog = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.wins = new System.Windows.Forms.Label();
+            this.losses = new System.Windows.Forms.Label();
+            this.switchPanel = new System.Windows.Forms.Panel();
+            this.labelswitch = new System.Windows.Forms.Label();
+            this.choiceNo = new System.Windows.Forms.Button();
+            this.choiceYes = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PickADoor = new System.Windows.Forms.Label();
+            this.choice3btn = new System.Windows.Forms.Button();
+            this.choice2btn = new System.Windows.Forms.Button();
+            this.choice1btn = new System.Windows.Forms.Button();
+            this.switchPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(12, 58);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(117, 108);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resultsList
+            // 
+            this.resultsList.FormattingEnabled = true;
+            this.resultsList.Location = new System.Drawing.Point(3, 8);
+            this.resultsList.Name = "resultsList";
+            this.resultsList.Size = new System.Drawing.Size(218, 43);
+            this.resultsList.TabIndex = 6;
+            // 
+            // activityLog
+            // 
+            this.activityLog.FormattingEnabled = true;
+            this.activityLog.Location = new System.Drawing.Point(227, 8);
+            this.activityLog.Name = "activityLog";
+            this.activityLog.Size = new System.Drawing.Size(181, 43);
+            this.activityLog.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Win";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(147, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Losses";
+            // 
+            // wins
+            // 
+            this.wins.AutoSize = true;
+            this.wins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wins.Location = new System.Drawing.Point(96, 238);
+            this.wins.Name = "wins";
+            this.wins.Size = new System.Drawing.Size(21, 24);
+            this.wins.TabIndex = 10;
+            this.wins.Text = "0";
+            // 
+            // losses
+            // 
+            this.losses.AutoSize = true;
+            this.losses.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.losses.Location = new System.Drawing.Point(173, 238);
+            this.losses.Name = "losses";
+            this.losses.Size = new System.Drawing.Size(21, 24);
+            this.losses.TabIndex = 11;
+            this.losses.Text = "0";
+            // 
+            // switchPanel
+            // 
+            this.switchPanel.Controls.Add(this.labelswitch);
+            this.switchPanel.Controls.Add(this.choiceNo);
+            this.switchPanel.Controls.Add(this.choiceYes);
+            this.switchPanel.Location = new System.Drawing.Point(265, 195);
+            this.switchPanel.Name = "switchPanel";
+            this.switchPanel.Size = new System.Drawing.Size(247, 100);
+            this.switchPanel.TabIndex = 12;
+            this.switchPanel.Visible = false;
+            // 
+            // labelswitch
+            // 
+            this.labelswitch.AutoSize = true;
+            this.labelswitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelswitch.Location = new System.Drawing.Point(13, 10);
+            this.labelswitch.Name = "labelswitch";
+            this.labelswitch.Size = new System.Drawing.Size(223, 24);
+            this.labelswitch.TabIndex = 2;
+            this.labelswitch.Text = "Do you want to switch?";
+            // 
+            // choiceNo
+            // 
+            this.choiceNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choiceNo.Location = new System.Drawing.Point(140, 46);
+            this.choiceNo.Name = "choiceNo";
+            this.choiceNo.Size = new System.Drawing.Size(89, 46);
+            this.choiceNo.TabIndex = 1;
+            this.choiceNo.Text = "No";
+            this.choiceNo.UseVisualStyleBackColor = true;
+            this.choiceNo.Click += new System.EventHandler(this.choiceNo_Click);
+            // 
+            // choiceYes
+            // 
+            this.choiceYes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choiceYes.Location = new System.Drawing.Point(17, 46);
+            this.choiceYes.Name = "choiceYes";
+            this.choiceYes.Size = new System.Drawing.Size(90, 46);
+            this.choiceYes.TabIndex = 0;
+            this.choiceYes.Text = "Yes";
+            this.choiceYes.UseVisualStyleBackColor = true;
+            this.choiceYes.Click += new System.EventHandler(this.choiceYes_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.resultsList);
+            this.panel2.Controls.Add(this.activityLog);
+            this.panel2.Location = new System.Drawing.Point(201, 306);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(429, 61);
+            this.panel2.TabIndex = 13;
+            // 
+            // PickADoor
+            // 
+            this.PickADoor.AutoSize = true;
+            this.PickADoor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PickADoor.Location = new System.Drawing.Point(304, 5);
+            this.PickADoor.Name = "PickADoor";
+            this.PickADoor.Size = new System.Drawing.Size(126, 24);
+            this.PickADoor.TabIndex = 3;
+            this.PickADoor.Text = "Pick A Door:";
+            this.PickADoor.Visible = false;
+            // 
+            // choice3btn
+            // 
+            this.choice3btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.choice3btn.Location = new System.Drawing.Point(458, 36);
+            this.choice3btn.Name = "choice3btn";
+            this.choice3btn.Size = new System.Drawing.Size(125, 153);
+            this.choice3btn.TabIndex = 5;
+            this.choice3btn.UseVisualStyleBackColor = true;
+            this.choice3btn.Click += new System.EventHandler(this.choice3btn_Click);
+            // 
+            // choice2btn
+            // 
+            this.choice2btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.choice2btn.Location = new System.Drawing.Point(305, 39);
+            this.choice2btn.Name = "choice2btn";
+            this.choice2btn.Size = new System.Drawing.Size(125, 153);
+            this.choice2btn.TabIndex = 4;
+            this.choice2btn.UseVisualStyleBackColor = true;
+            this.choice2btn.Click += new System.EventHandler(this.choice2btn_Click);
+            // 
+            // choice1btn
+            // 
+            this.choice1btn.Location = new System.Drawing.Point(151, 36);
+            this.choice1btn.Name = "choice1btn";
+            this.choice1btn.Size = new System.Drawing.Size(125, 153);
+            this.choice1btn.TabIndex = 3;
+            this.choice1btn.UseVisualStyleBackColor = true;
+            this.choice1btn.Click += new System.EventHandler(this.choice1btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 299);
+            this.Controls.Add(this.PickADoor);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.switchPanel);
+            this.Controls.Add(this.losses);
+            this.Controls.Add(this.wins);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.choice3btn);
+            this.Controls.Add(this.choice2btn);
+            this.Controls.Add(this.choice1btn);
+            this.Controls.Add(this.startButton);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Car And Donkey Game";
+            this.switchPanel.ResumeLayout(false);
+            this.switchPanel.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button choice1btn;
+        private System.Windows.Forms.Button choice2btn;
+        private System.Windows.Forms.Button choice3btn;
+        private System.Windows.Forms.ListBox resultsList;
+        private System.Windows.Forms.ListBox activityLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label wins;
+        private System.Windows.Forms.Label losses;
+        private System.Windows.Forms.Panel switchPanel;
+        private System.Windows.Forms.Label labelswitch;
+        private System.Windows.Forms.Button choiceNo;
+        private System.Windows.Forms.Button choiceYes;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label PickADoor;
+    }
+}
+
---
+++ Car and Donkey/Form1.cs
@@ -0,0 +1,537 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_and_Donkey
+{
+    public partial class Form1 : Form
+    {
+
+        bool picking = false;
+        int correctDoor;
+
+        string poss1 = "CDD";
+        string poss2 = "DCD";
+        string poss3 = "DDC";
+
+        bool poss1going;
+        bool poss2going;
+        bool poss3going;
+
+        int userChoice;
+        int flippedDoor;
+
+        int winsCtr;
+        int lossesCtr;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RunSimulation(int timesToRun)
+        {
+
+            Simulation_part1();
+
+
+        }
+
+        private void reset()
+        {
+            choice1btn.BackColor = Color.Gray;
+            choice2btn.BackColor = Color.Gray;
+            choice3btn.BackColor = Color.Gray;
+            activityLog.Items.Clear();
+            resultsList.Items.Clear();
+            poss1going = false;
+            poss2going = false;
+            poss3going = false;
+
+            choice1btn.BackgroundImage = null;
+            choice2btn.BackgroundImage = null;
+            choice3btn.BackgroundImage = null;
+
+            switchPanel.Visible = false;
+
+            PickADoor.Text = "PICK A DOOR";
+        }
+
+        
+
+        private void Simulation_part1()
+        {
+            reset();
+            startButton.Enabled = false;
+
+            PickADoor.Visible = true;
+
+            var rndNumber = new Random();
+
+            correctDoor = rndNumber.Next(1, 4);
+
+            LogActivity("CORRECT DOOR IS: " + correctDoor.ToString());
+            if (correctDoor == 1)
+            {
+                poss1going = true;
+                LogActivity("THE POSSIBILITY GOING IS: " + poss1going);
+
+            }
+            else if (correctDoor == 2)
+            {
+                poss2going = true;
+                LogActivity("THE POSSIBILITY GOING IS: " + poss2going);
+
+            }
+            else
+            {
+                poss3going = true;
+                LogActivity("THE POSSIBILITY GOING IS: " + poss3going);
+
+            }
+
+           
+            picking = true;
+        }
+
+        private void Simulation_part2(int doorChoice)
+        {
+            if (poss1going)
+            {
+                var furtherRandom = new Random();
+                int doorToReveal = furtherRandom.Next(2, 4);
+                flippedDoor = doorToReveal;
+
+                LogActivity("The flipped door is: " + flippedDoor.ToString());
+                if (doorChoice == 1)
+                {
+                    
+                    if (doorToReveal == 2)
+                    {
+                        //choice2btn.Text = "DONKEY";
+                        choice2btn.BackgroundImage = Properties.Resources.donkey1;
+                    } else
+                    {
+                        //choice3btn.Text = "DONKEY";
+                        choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+                    }
+                }
+                else if(doorChoice == 2)
+                {
+                        //choice3btn.Text = "DONKEY";
+                    choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+                }
+                else
+                {
+                       // choice2btn.Text = "DONKEY";
+                    choice2btn.BackgroundImage = Properties.Resources.donkey1;
+
+                }
+            }
+            else if (poss2going)
+            {
+                var furtherRandom = new Random();
+                int doorToReveal = furtherRandom.Next(2, 4);
+                flippedDoor = doorToReveal;
+                LogActivity("The flipped door is: " + flippedDoor.ToString());
+
+                if (doorChoice == 2)
+                {
+                    
+
+                    if (doorToReveal == 2)
+                    {
+                        //choice1btn.Text = "DONKEY";
+                        choice1btn.BackgroundImage = Properties.Resources.donkey1;
+
+                    }
+                    else
+                    {
+                        //choice3btn.Text = "DONKEY";
+                        choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+                    }
+                }
+                else if (doorChoice == 1)
+                {
+                    //choice3btn.Text = "DONKEY";
+                    choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+                }
+                else
+                {
+                    //choice1btn.Text = "DONKEY";
+                    choice1btn.BackgroundImage = Properties.Resources.donkey1;
+
+                }
+            }
+            else if (poss3going)
+            {
+                var furtherRandom = new Random();
+                int doorToReveal = furtherRandom.Next(2, 4);
+                flippedDoor = doorToReveal;
+                LogActivity("The flipped door is: " + flippedDoor.ToString());
+
+                if (doorChoice == 3)
+                {
+                   
+
+                    if (doorToReveal == 2)
+                    {
+                        choice1btn.BackgroundImage = Properties.Resources.donkey1;
+
+                        //choice1btn.Text = "DONKEY";
+                    }
+                    else
+                    {
+                        choice2btn.BackgroundImage = Properties.Resources.donkey1;
+
+                        //choice2btn.Text = "DONKEY";
+                    }
+                }
+                else if (doorChoice == 1)
+                {
+                    choice2btn.BackgroundImage = Properties.Resources.donkey1;
+
+                    //choice2btn.Text = "DONKEY";
+                }
+                else
+                {
+                    choice1btn.BackgroundImage = Properties.Resources.donkey1;
+
+                    //choice1btn.Text = "DONKEY";
+                }
+            }
+            wannaSwitch();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            RunSimulation(5);
+        }
+
+        private void chooseDoor(int doorChoice)
+        {
+
+        }
+
+        private void LogActivity(string activity)
+        {
+            activityLog.Items.Add(activity);
+        }
+
+        private void choice1btn_Click(object sender, EventArgs e)
+        {
+            if (picking)
+            {
+                choice1btn.BackColor = Color.Green;
+                userChoice = 1;
+                Simulation_part2(1);
+
+            }
+
+
+        }
+
+        private void choice2btn_Click(object sender, EventArgs e)
+        {
+            if (picking)
+            {
+                choice2btn.BackColor = Color.Green;
+
+                userChoice = 2;
+
+                Simulation_part2(2);
+
+            }
+
+        }
+
+        private void choice3btn_Click(object sender, EventArgs e)
+        {
+            if (picking)
+            {
+                choice3btn.BackColor = Color.Green;
+
+                userChoice = 3;
+
+                Simulation_part2(3);
+
+            }
+
+        }
+
+        
+
+        private void endGame(string condition)
+        {
+            startButton.Enabled = true;
+
+            if (condition == "win")
+            {
+                playerWins();
+            }
+            else
+            {
+                playerLoses();
+            }
+        }
+        private void playerWins()
+        {
+            picking = false;
+            switchPanel.Visible = false;
+            resultsList.Items.Add("you win");
+            PickADoor.Text = "YOU WIN!";
+            winsCtr += 1;
+            wins.Text = winsCtr.ToString();
+
+            using (var player = new SoundPlayer(Properties.Resources.carstart))
+            {
+                player.Play();
+            }
+
+        }
+
+        private void playerLoses()
+        {
+            picking = false;
+
+            PickADoor.Text = "YOU LOSE!";
+            switchPanel.Visible = false;
+
+            resultsList.Items.Add("you lost");
+            lossesCtr += 1;
+            losses.Text = lossesCtr.ToString();
+            using (var player = new SoundPlayer(Properties.Resources.donkey))
+            {
+                player.Play();
+            }
+        }
+
+
+        private void wannaSwitch()
+        {
+            switchPanel.Visible = true;
+
+            /*DialogResult dialogResult = MessageBox.Show("Want to switch?", "Want to switch?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (poss1going)
+                {
+                    if (userChoice == 1)
+                    {
+                        playerLoses();
+                    }
+                    else
+                    {
+                        playerWins();
+
+                    }
+
+                    choice1btn.Text = "CAR";
+                    choice1btn.BackgroundImage = Properties.Resources.car;
+                    choice2btn.BackgroundImage = Properties.Resources.donkey1;
+                    choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+                    choice2btn.Text = "DONKEY";
+                    choice3btn.Text = "DONKEY";
+
+                }
+                if (poss2going)
+                {
+                    if (userChoice == 2)
+                    {
+                        playerLoses();
+
+
+                    }
+                    else
+                    {
+                        playerWins();
+
+                    }
+
+                    choice1btn.Text = "DONKEY";
+                    choice2btn.Text = "CAR";
+                    choice3btn.Text = "DONKEY";
+
+                    choice2btn.BackgroundImage = Properties.Resources.car;
+                    choice1btn.BackgroundImage = Properties.Resources.donkey1;
+                    choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+                }
+                if (poss3going)
+                {
+                    if (userChoice == 3)
+                    {
+                        playerLoses();
+
+
+                    }
+                    else
+                    {
+                        playerWins();
+
+                    }
+
+                    choice1btn.Text = "DONKEY";
+                    choice2btn.Text = "DONKEY";
+                    choice3btn.Text = "CAR";
+
+                    choice3btn.BackgroundImage = Properties.Resources.car;
+                    choice2btn.BackgroundImage = Properties.Resources.donkey1;
+                    choice1btn.BackgroundImage = Properties.Resources.donkey1;
+
+                }
+
+
+                //do something
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+            */
+        }
+
+        private void choiceYes_Click(object sender, EventArgs e)
+        {
+            if (poss1going)
+            {
+                if (userChoice == 1)
+                {
+                    endGame("lose");
+                }
+                else
+                {
+                    endGame("win");
+
+                }
+
+                
+                choice1btn.BackgroundImage = Properties.Resources.car;
+                choice2btn.BackgroundImage = Properties.Resources.donkey1;
+                choice3btn.BackgroundImage = Properties.Resources.donkey1;
+                
+
+            }
+            if (poss2going)
+            {
+                if (userChoice == 2)
+                {
+                    endGame("lose");
+
+
+                }
+                else
+                {
+                    endGame("win");
+
+                }
+
+                
+
+                choice2btn.BackgroundImage = Properties.Resources.car;
+                choice1btn.BackgroundImage = Properties.Resources.donkey1;
+                choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+            }
+            if (poss3going)
+            {
+                if (userChoice == 3)
+                {
+                    endGame("lose");
+
+
+                }
+                else
+                {
+                    endGame("win");
+
+                }
+
+                
+
+                choice3btn.BackgroundImage = Properties.Resources.car;
+                choice2btn.BackgroundImage = Properties.Resources.donkey1;
+                choice1btn.BackgroundImage = Properties.Resources.donkey1;
+
+            }
+        }
+
+        private void choiceNo_Click(object sender, EventArgs e)
+        {
+            if (poss1going)
+            {
+                if (userChoice == 1)
+                {
+                    endGame("win");
+                    
+                }
+                else
+                {
+                    endGame("lose");
+
+                }
+
+                
+                choice1btn.BackgroundImage = Properties.Resources.car;
+                choice2btn.BackgroundImage = Properties.Resources.donkey1;
+                choice3btn.BackgroundImage = Properties.Resources.donkey1;
+                
+
+            }
+            if (poss2going)
+            {
+                if (userChoice == 2)
+                {
+                    endGame("win");
+
+
+                }
+                else
+                {
+                    endGame("lose");
+                }
+
+                
+
+                choice2btn.BackgroundImage = Properties.Resources.car;
+                choice1btn.BackgroundImage = Properties.Resources.donkey1;
+                choice3btn.BackgroundImage = Properties.Resources.donkey1;
+
+            }
+            if (poss3going)
+            {
+                if (userChoice == 3)
+                {
+                    endGame("win");
+
+
+                }
+                else
+                {
+                    endGame("lose");
+                }
+
+                
+
+                choice3btn.BackgroundImage = Properties.Resources.car;
+                choice2btn.BackgroundImage = Properties.Resources.donkey1;
+                choice1btn.BackgroundImage = Properties.Resources.donkey1;
+
+            }
+        }
+    }
+}
